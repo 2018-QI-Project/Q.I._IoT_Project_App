3c9109c975aca71d8faf6967db3216232b2201d7
MSChoi <yuuhio9681@gmail.com>
2018-07-18 16:46:55 -0700

    Adjust Bluetooth

--- app/src/main/java/qualcomminstitute/iot/BluetoothDialog.java
+++ app/src/main/java/qualcomminstitute/iot/BluetoothDialog.java
@@ -2,12 +2,12 @@ package qualcomminstitute.iot;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 
-public class BluetoothConnectActivity extends AppCompatActivity {
+public class BluetoothDialog {
+    private final String BLUETOOTH_UUID = "00001101-0000-1000-8000-00805F9B34FB";
     private BluetoothAdapter bluetooth;
 
-    public BluetoothConnectActivity() {
+    public BluetoothDialog() {
         bluetooth = BluetoothAdapter.getDefaultAdapter();
     }
 
@@ -17,7 +17,6 @@ public class BluetoothConnectActivity extends AppCompatActivity {
         }
         else {
             Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(intent);
         }
         return bluetooth.isEnabled();
     }
