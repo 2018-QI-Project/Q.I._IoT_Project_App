a9c48eaf8ad3e045f27213536b1ec9eaf7d30101
MSChoi <yuuhio9681@gmail.com>
2018-07-23 11:58:36 -0700

    User Interface (Sign In / Forgot Password)

---
+++ app/src/main/java/qualcomminstitute/iot/ForgotPasswordActivity.java
@@ -0,0 +1,14 @@
+package qualcomminstitute.iot;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
--- app/src/main/java/qualcomminstitute/iot/MainActivity.java
+++ app/src/main/java/qualcomminstitute/iot/MainActivity.java
@@ -1,13 +1,25 @@
 package qualcomminstitute.iot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtForgotPassword = findViewById(R.id.txtSignInForgotPassword);
+        txtForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
