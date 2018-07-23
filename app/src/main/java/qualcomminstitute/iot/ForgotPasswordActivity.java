36a177bbdda429d2682005479d33c02cad5fdbfe
MSChoi <yuuhio9681@gmail.com>
2018-07-23 14:49:30 -0700

    Sign Up Interface

--- app/src/main/java/qualcomminstitute/iot/ForgotPasswordActivity.java
+++ app/src/main/java/qualcomminstitute/iot/ForgotPasswordActivity.java
@@ -1,14 +1,23 @@
 package qualcomminstitute.iot;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        Button btnSubmit = (Button)findViewById(R.id.btnResetPasswordSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/qualcomminstitute/iot/MainActivity.java
+++ app/src/main/java/qualcomminstitute/iot/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtForgotPassword = findViewById(R.id.txtSignInForgotPassword);
+        // Sign Up Button Click Event Listener
+        Button btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Forgot Password Click Event Listener
+        TextView txtForgotPassword = (TextView)findViewById(R.id.txtSignInForgotPassword);
         txtForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
